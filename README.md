6931668af73902c69a034bad98534379df4860f6
Steven Loe <stevenjloe@protonmail.com>
2020-08-23 17:44:39 -0400

    Get data from API

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Project is created with:
 To run this project, install it locally using npm:
 
 ```
-$ cd ../project
+$ cd ../eleventy-site-gen-demo
 $ npm install
 $ npm start
 ```
---
+++ src/_data/news.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const axios = require("axios");
+
+;
+
+module.exports = async function() {
+  try {
+    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
